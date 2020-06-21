e7a548e56903479c467de8fe62c66af50120f0e6
minhphuongpt <phamminhphuongstudent@gmail.com>
2020-06-21 10:14:57 +0700

    Add navBar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import Nav from './modules/Nav/pages/navs';
 import './App.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+        <Nav />
+      </body>
     </div>
   );
 }
