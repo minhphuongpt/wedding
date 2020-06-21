cc7732f80a01d504eb4ba312b92fac5f24c4163b
minhphuongpt <phamminhphuongstudent@gmail.com>
2020-06-21 11:39:57 +0700

    modify Appjs

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Tutorial
+          Learn React
         </a>
       </header>
     </div>
